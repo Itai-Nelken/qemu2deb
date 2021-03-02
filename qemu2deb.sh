8589920bd4600fd4dde12c9d8bfbd885436d5781
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-03-02 20:52:52 +0200

    critical fix
    ...to files copied, the script copied basch comletions which is in package 'bash completions'.

--- qemu2deb.sh
+++ qemu2deb.sh
@@ -294,8 +294,8 @@ function make-deb() {
     echo -ne '(0%)[########                  ](100%)\r'
     mkdir -p usr/local/share/
     cp -r /usr/local/share/qemu/ $DIRECTORY/qemu-$QVER-$ARCH/usr/local/share
-    mkdir -p usr/share/bash-completion/completions/
-    cp /usr/share/bash-completion/completions/qemu* $DIRECTORY/qemu-$QVER-$ARCH/usr/share/bash-completion/completions/
+    #mkdir -p usr/share/bash-completion/completions/
+    #cp /usr/share/bash-completion/completions/qemu* $DIRECTORY/qemu-$QVER-$ARCH/usr/share/bash-completion/completions/
     mkdir -p usr/local/share/applications
     cp /usr/local/share/applications/qemu.desktop $DIRECTORY/qemu-$QVER-$ARCH/usr/local/share/applications/
     sleep 0.1
